fdbac942c362148baa3ce9ce03846b10e9ec1ce1
gajaharan <gajaharan@gmail.com>
2017-04-29 16:46:15 +0100

    Initial Commit

---
+++ BrickBreaker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BrickBreaker
+//
+//  Created by Gajaharan Satkunanandan on 29/04/2017.
+//  Copyright © 2017 Gajaharan Satkunanandan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BrickBreaker/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  BrickBreaker
+//
+
+//  Copyright (c) 2017 Gajaharan Satkunanandan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
